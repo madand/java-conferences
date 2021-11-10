0a03fdba5777ce802c832698937e56339637b9f6
madand <dev@madand.net>
2021-11-10 23:12:17 +0200

    Improve schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,12 +2,12 @@
 -- Clean-up the database
 DROP TABLE IF EXISTS talk_speaker_request;
 DROP TABLE IF EXISTS talk_speaker_proposal;
-DROP TABLE IF EXISTS new_talk_proposal_lang;
+DROP TABLE IF EXISTS new_talk_proposal_l10n;
 DROP TABLE IF EXISTS new_talk_proposal;
-DROP TABLE IF EXISTS talk_lang;
+DROP TABLE IF EXISTS talk_l10n;
 DROP TABLE IF EXISTS talk;
 DROP TABLE IF EXISTS conference_attendee;
-DROP TABLE IF EXISTS conference_lang;
+DROP TABLE IF EXISTS conference_l10n;
 DROP TABLE IF EXISTS conference;
 DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS language;
@@ -29,7 +29,7 @@ CREATE TABLE "user" (
 
 CREATE TABLE language (
   id serial NOT NULL PRIMARY KEY,
-  code character varying(6) NOT NULL,
+  code character varying(6) NOT NULL UNIQUE,
   name character varying(20) NOT NULL,
   is_default boolean NOT NULL DEFAULT false
 );
@@ -42,7 +42,7 @@ CREATE TABLE conference (
   actually_attended_count integer
 );
 
-CREATE TABLE conference_lang (
+CREATE TABLE conference_l10n (
   conference_id integer NOT NULL REFERENCES conference(id) ON UPDATE CASCADE ON DELETE CASCADE,
   language_id integer NOT NULL REFERENCES language(id) ON UPDATE CASCADE ON DELETE CASCADE,
   name character varying(255) NOT NULL,
@@ -53,26 +53,29 @@ CREATE TABLE conference_lang (
 
 CREATE TABLE conference_attendee (
   id serial NOT NULL PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   conference_id integer NOT NULL REFERENCES conference(id) ON UPDATE CASCADE ON DELETE CASCADE,
   user_id integer NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+  UNIQUE (conference_id, user_id)
 );
 COMMENT ON TABLE conference_attendee
   IS 'Fact that the user will attend the conference.';
 
 CREATE TABLE talk (
   id serial NOT NULL PRIMARY KEY,
-  crated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  conference_id integer NOT NULL REFERENCES conference(id) ON UPDATE CASCADE ON DELETE CASCADE,
   speaker_id integer REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE SET NULL,
   start_time time without time zone NOT NULL,
   end_time time without time zone NOT NULL,
+  UNIQUE (conference_id, start_time),
   CHECK (start_time < end_time)
 );
-COMMENT ON TABLE conference_attendee
+COMMENT ON TABLE talk
   IS 'A talk given at the conference.';
 
-CREATE TABLE talk_lang (
+CREATE TABLE talk_l10n (
     talk_id integer NOT NULL REFERENCES talk(id) ON UPDATE CASCADE ON DELETE CASCADE,
     language_id integer NOT NULL REFERENCES language(id) ON UPDATE CASCADE ON DELETE CASCADE,
     name character varying(255) NOT NULL,
@@ -82,28 +85,28 @@ CREATE TABLE talk_lang (
 
 CREATE TABLE talk_speaker_request (
   id serial NOT NULL PRIMARY KEY,
-  crated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   talk_id integer NOT NULL REFERENCES talk(id) ON UPDATE CASCADE ON DELETE CASCADE,
   speaker_id integer NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+  UNIQUE (talk_id, speaker_id)
 );
 COMMENT ON TABLE talk_speaker_request
   IS 'Speaker''s request to hold the talk. Should be approved by the moderator.';
 
 CREATE TABLE talk_speaker_proposal (
   id serial NOT NULL PRIMARY KEY,
-  crated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   talk_id integer NOT NULL REFERENCES talk(id) ON UPDATE CASCADE ON DELETE CASCADE,
   speaker_id integer NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE,
   moderator_id integer NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+  UNIQUE (talk_id, speaker_id)
 );
 COMMENT ON TABLE talk_speaker_proposal
   IS 'Moderator''s proposal to the speaker to hold the talk. Should be accepted by the speaker.';
 
 CREATE TABLE new_talk_proposal (
   id serial NOT NULL PRIMARY KEY,
-  crated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   conference_id integer NOT NULL REFERENCES conference(id) ON UPDATE CASCADE ON DELETE CASCADE,
   speaker_id integer NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE SET NULL,
   duration integer NOT NULL
@@ -111,7 +114,7 @@ CREATE TABLE new_talk_proposal (
 COMMENT ON TABLE new_talk_proposal
   IS 'New talk proposed by the speaker. Should be accepted by a moderator.';
 
-CREATE TABLE new_talk_proposal_lang (
+CREATE TABLE new_talk_proposal_l10n (
   talk_proposal_id integer NOT NULL REFERENCES new_talk_proposal(id) ON UPDATE CASCADE ON DELETE CASCADE,
   language_id integer NOT NULL REFERENCES language(id) ON UPDATE CASCADE ON DELETE CASCADE,
   name character varying(255) NOT NULL,
