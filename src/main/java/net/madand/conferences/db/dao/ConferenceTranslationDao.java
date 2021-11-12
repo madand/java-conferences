7599096eb8072c45bce13b5b4883a4d140cddb19
madand <dev@madand.net>
2021-11-12 17:31:59 +0200

    ConferenceTranslation entity, its DAO and tests

---
+++ src/main/java/net/madand/conferences/db/dao/ConferenceTranslationDao.java
@@ -0,0 +1,74 @@
+package net.madand.conferences.db.dao;
+
+import net.madand.conferences.db.Mapper;
+import net.madand.conferences.db.QueryHelper;
+import net.madand.conferences.db.StatementParametersSetter;
+import net.madand.conferences.entity.Conference;
+import net.madand.conferences.entity.ConferenceTranslation;
+import net.madand.conferences.entity.Language;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class ConferenceTranslationDao {
+    private static final String SQL_FIND_ONE = "SELECT * FROM conference_translation WHERE conference_id = ? AND language_id = ?";
+    private static final String SQL_INSERT = "INSERT INTO conference_translation (conference_id, language_id, name, description, location) VALUES (?,?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE conference_translation SET " +
+            "conference_id = ?, language_id = ?, name = ?, description = ?, location = ? " +
+            "WHERE conference_id = ? AND language_id = ?";
+    private static final String SQL_DELETE = "DELETE FROM conference_translation WHERE conference_id = ? AND language_id = ?";
+
+    public static Optional<ConferenceTranslation> findOne(Connection conn, Conference conference, Language language) throws SQLException {
+        return QueryHelper.findOne(conn, SQL_FIND_ONE, stmt -> {
+                    stmt.setInt(1, conference.getId());
+                    stmt.setInt(2, language.getId());
+                },
+                makeRowMapper(conference, language));
+    }
+
+    public static void insert(Connection conn, ConferenceTranslation conferenceTranslation) throws SQLException {
+        QueryHelper.insert(conn, SQL_INSERT, makeInsertUpdateParametersSetter(conferenceTranslation));
+    }
+
+    public static void update(Connection conn, ConferenceTranslation conferenceTranslation) throws SQLException {
+        QueryHelper.update(conn, SQL_UPDATE, stmt -> {
+            makeInsertUpdateParametersSetter(conferenceTranslation).setStatementParameters(stmt);
+            stmt.setInt(6, conferenceTranslation.getConference().getId());
+            stmt.setInt(7, conferenceTranslation.getLanguage().getId());
+        });
+    }
+
+    public static void delete(Connection conn, ConferenceTranslation conferenceTranslation) throws SQLException {
+        QueryHelper.delete(conn, SQL_DELETE, stmt -> {
+            stmt.setInt(1, conferenceTranslation.getConference().getId());
+            stmt.setInt(2, conferenceTranslation.getLanguage().getId());
+        });
+    }
+
+    private static StatementParametersSetter makeInsertUpdateParametersSetter(ConferenceTranslation conferenceTranslation) {
+        return (stmt) -> {
+            int i = 0;
+            stmt.setInt(++i, conferenceTranslation.getConference().getId());
+            stmt.setInt(++i, conferenceTranslation.getLanguage().getId());
+            stmt.setString(++i, conferenceTranslation.getName());
+            stmt.setString(++i, conferenceTranslation.getDescription());
+            stmt.setString(++i, conferenceTranslation.getLocation());
+        };
+    }
+
+    private static Mapper<ConferenceTranslation> makeRowMapper(Conference conference, Language language) throws SQLException {
+        return (rs) -> {
+            ConferenceTranslation conferenceTranslation = new ConferenceTranslation();
+            conferenceTranslation.setConference(conference);
+            conferenceTranslation.setLanguage(language);
+
+            int i = 2; // Skip the first two rows, with conference_id and language_id.
+            conferenceTranslation.setName(rs.getString(++i));
+            conferenceTranslation.setDescription(rs.getString(++i));
+            conferenceTranslation.setLocation(rs.getString(++i));
+
+            return conferenceTranslation;
+        };
+    }
+}
---
+++ src/main/java/net/madand/conferences/entity/ConferenceTranslation.java
@@ -0,0 +1,89 @@
+package net.madand.conferences.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class ConferenceTranslation implements Serializable {
+    private static final long serialVersionUID = -2505015520200827678L;
+
+    private Conference conference;
+    private Language language;
+    private String name;
+    private String description;
+    private String location;
+
+    /**
+     * Static factory of {@link ConferenceTranslation} instances.
+     *
+     * @param conference
+     * @param language
+     * @param name
+     * @param description
+     * @param location
+     * @return
+     */
+    public static ConferenceTranslation makeInstance(Conference conference, Language language, String name,
+                                                     String description, String location) {
+        ConferenceTranslation conferenceTranslation = new ConferenceTranslation();
+        conferenceTranslation.setConference(conference);
+        conferenceTranslation.setLanguage(language);
+        conferenceTranslation.setName(name);
+        conferenceTranslation.setDescription(description);
+        conferenceTranslation.setLocation(location);
+        return conferenceTranslation;
+    }
+
+    public Conference getConference() {
+        return conference;
+    }
+
+    public void setConference(Conference conference) {
+        this.conference = conference;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConferenceTranslation that = (ConferenceTranslation) o;
+        return conference.equals(that.conference) && language.equals(that.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(conference, language);
+    }
+}
